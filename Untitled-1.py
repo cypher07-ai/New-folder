7c049f7e6e0d330d2f787089133b0b612e92dec0
ayan kaushik <ayanrunning99@gmail.com>
2026-09-13 11:57:59 +0530

    dict prac

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,19 @@
-a = { 1:"b" , 2:"c" , 3:"d"
-      }
-a[1] = "lauda"
-print(a[1])
-a.update({4:"e", 5:"f"})
+d = {'eggs': 2, 'milk': 1, 'bread': 3}
+d2 = {'apples': 5, 'oranges': 2, 'bananas': 4}
+d.update(d2)
+print(d)
+print(len(d))
+print('eggs' in d)
+print(list(d.keys()))
+print(list(d.values()))
+print(d.get('milk'))
+print(d.items())
+print(d.get('toast'))
+d = {'eggs': 2, 'milk': 1, 'bread': 3}
+print(d.pop('milk'))
+print(d)
+a = d.popitem()
 print(a)
+print(d)
+b = str(d)
+print(b)
